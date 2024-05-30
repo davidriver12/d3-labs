5e07fd31c3d508a769eb67133d3991cfc80a2013
davidriver12 <dav-rc@hotmail.com>
2024-05-29 22:59:51 -0600

    Final exercises

--- ex5/js/main.js
+++ ex5/js/main.js
@@ -11,31 +11,67 @@ const data = [
 	{ "name": "China Zun", "height": "528", "country": "China" }
 ];
 
+const margin = { left: 100, right: 10, top: 10, bottom: 100 };
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
 const svg = d3.select("#chart-area").append("svg")
 	.attr("width", 600)
-	.attr("height", 400)
+	.attr("height", 400);
+
+const g = svg.append("g")
+	.attr("transform", `translate(${margin.left},${margin.top})`);
 
 const x = d3.scaleBand()
 	.domain(data.map(d => d.name))
-	.range([0, 400])
+	.range([0, width])
 	.paddingInner(0.3)
 	.paddingOuter(0.3);
 
 const y = d3.scaleLinear()
 	.domain([0, 828])
-	.range([400, 0]);
+	.range([height, 0]);
 
 const color = d3.scaleOrdinal()
 	.domain(data.map(d => d.name))
 	.range(d3.schemeSet3);
 
-svg.selectAll(".bar")
-	.data(data)
-	.enter()
+var rects = g.selectAll("rect").data(data);
+
+rects.enter()
 	.append("rect")
 	.attr("class", "bar")
 	.attr("x", d => x(d.name))
 	.attr("y", d => y(d.height))
 	.attr("width", x.bandwidth())
-	.attr("height", d => 400 - y(d.height))
+	.attr("height", d => height - y(d.height))
 	.attr("fill", d => color(d.name));
+
+g.append("g")
+	.attr("class", "x-axis")
+	.attr("transform", `translate(0,${height})`)
+	.call(d3.axisBottom(x))
+	.selectAll("text")
+	.attr("transform", "rotate(-40)")
+	.attr("x", -5)
+	.attr("y", 10)
+	.style("text-anchor", "end");
+
+g.append("g")
+	.attr("class", "y-axis")
+	.call(d3.axisLeft(y).ticks(5).tickFormat(d => `${d}m`));
+
+g.append("text")
+	.attr("class", "axis-label")
+	.attr("x", width / 2)
+	.attr("y", height + 140)
+	.attr("text-anchor", "middle")
+	.text("The World's Tallest Buildings");
+
+g.append("text")
+	.attr("class", "axis-label")
+	.attr("x", -(height / 2))
+	.attr("y", -60)
+	.attr("text-anchor", "middle")
+	.attr("transform", "rotate(-90)")
+	.text("Height (m)");
---
+++ ex6/js/main.js
@@ -0,0 +1,58 @@
+d3.json("data/revenues.json").then(function(data) {
+
+    var margin = {top: 20, right: 20, bottom: 70, left: 70},
+        width = 600 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    var svg = d3.select("body")
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", 
+                      "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleBand()
+              .domain(data.map(function(d) { return d.month; }))
+              .range([0, width])
+              .padding(0.1);
+
+    var y = d3.scaleLinear()
+              .domain([0, d3.max(data, function(d) { return +d.revenue; })])
+              .range([height, 0]);
+
+    svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(x))
+       .selectAll("text")
+       .style("text-anchor", "end")
+       .attr("dx", "-0.5em")
+       .attr("dy", "0.15em")
+       .attr("transform", "rotate(-65)");
+
+    svg.append("g")
+       .call(d3.axisLeft(y));
+
+    svg.selectAll(".bar")
+       .data(data)
+       .enter().append("rect")
+       .attr("class", "bar")
+       .attr("x", function(d) { return x(d.month); })
+       .attr("width", x.bandwidth())
+       .attr("y", function(d) { return y(+d.revenue); })
+       .attr("height", function(d) { return height - y(+d.revenue); });
+
+    svg.append("text")
+       .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 20) + ")")
+       .style("text-anchor", "middle")
+       .text("Month");
+
+    svg.append("text")
+       .attr("transform", "rotate(-90)")
+       .attr("y", 0 - margin.left)
+       .attr("x", 0 - (height / 2))
+       .attr("dy", "1em")
+       .style("text-anchor", "middle")
+       .text("Revenue");
+
+});
